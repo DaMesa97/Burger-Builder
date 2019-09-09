fb9b43156105324d9394ac0f55ed79dfce0866b4
Tymoteusz Weglorz <tymoteusz.weglorz@gmail.com>
2019-09-09 13:03:13 +0100

    Create toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => {
-    return (
-        <Aux>
-            <div>Toolbar, SideDrawer, BackDrop</div>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    );
+   return (
+      <Aux>
+         < Toolbar />
+         <main className={classes.Content}>
+            {props.children}
+         </main>
+      </Aux>
+   );
 }
 
 export default layout
