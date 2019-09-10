9377de33e2993fefbc5af95b130b20789e370116
Tymoteusz Weglorz <tymoteusz.weglorz@gmail.com>
2019-09-10 12:38:17 +0100

    add logo

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navigationItem = (props) => {
+   return (
+      <div></div>
+   );
+}
+
+export default navigationItem
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navigationItems = (props) => {
+   return (
+      <ul>
+         <li></li>
+      </ul>
+   );
+}
+
+export default navigationItems
