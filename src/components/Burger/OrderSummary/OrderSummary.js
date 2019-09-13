6c8e40e3093892c8c2275e22c75cfb76af5da05c
Tymoteusz Weglorz <tymoteusz.weglorz@gmail.com>
2019-09-13 13:55:33 +0100

    optimalize lifecycle hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-   const ingredientSummary = Object.keys(props.ingredients)
-      .map(igKey => {
-         return <li key={igKey}><span style={{ textTransform: "capitalize" }}>{igKey}</span>: {props.ingredients[igKey]}</li>
-      })
-   return (
-      <Aux>
-         <h3>Your order</h3>
-         <p>Delicious burger with: </p>
-         <ul>
-            {ingredientSummary}
-         </ul>
-         <h3>Your total: {Math.floor(props.price * 100) / 100}$</h3>
-         <p>Continue to checkout?</p>
-         <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-         <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-      </Aux>
-   );
+class OrderSummary extends Component {
+   render() {
+      const ingredientSummary = Object.keys(this.props.ingredients)
+         .map(igKey => {
+            return <li key={igKey}><span style={{ textTransform: "capitalize" }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+         })
+      return (
+         <Aux>
+            <h3>Your order</h3>
+            <p>Delicious burger with: </p>
+            <ul>
+               {ingredientSummary}
+            </ul>
+            <h3>Your total: {Math.floor(this.props.price * 100) / 100}$</h3>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+         </Aux >
+      );
+   }
 }
 
-export default orderSummary
+export default OrderSummary
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Aux'
+import classes from './Layout.module.css'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+
+   state = {
+      showSideDrawer: false
+   }
+
+   sideDrawerToggleHandler = () => {
+      let show = this.state.showSideDrawer
+      this.setState({
+         showSideDrawer: !show
+      })
+   }
+
+   render() {
+      return (
+         <Aux>
+            < Toolbar toggle={this.sideDrawerToggleHandler} />
+            < SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerToggleHandler} />
+            <main className={classes.Content}>
+               {this.props.children}
+            </main>
+         </Aux>
+      );
+   }
+}
+
+export default Layout
